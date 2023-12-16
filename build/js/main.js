c7eca996a369ca778b768f5bcd970b4d2efded95
@adrianfontanilla <@adrianfontanilla>
2023-12-15 19:16:47 -0600

    implement total reviews and most recent reviewer

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,30 @@
 "use strict";
+const reviewTotalDisplay = document.querySelector('#reviews');
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021',
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021',
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021',
+    },
+];
+function showReviewTotal(value) {
+    const result = reviews.reduce((r, o) => (o.date > r.date ? o : r));
+    if (reviewTotalDisplay !== null) {
+        reviewTotalDisplay.innerHTML =
+            'review total ' + value.toString() + '| last reviewed by' + result.name;
+    }
+}
+showReviewTotal(reviews.length);
